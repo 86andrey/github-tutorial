e96cd030faf5b9a27fa110ed6d542a58d9943569
Andrey Ivanov <prioritet2503@gmail.com>
2022-05-25 10:51:01 +0300

    save

---
+++ script.js
@@ -0,0 +1,21 @@
+const startBtn = document.querySelector(".js-start");
+const stopBtn = document.querySelector(".js-stop");
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+    console.log(`I love async JS!  ${Math.random()}`);
+  }, 1000);
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+  console.log(`Interval with id ${timerId} has stopped!`);
+});
+const date = new Date();
+
+console.log(date);
+// "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
+
+console.log(date.toString());
+// "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import name from './module-1';
-import '.css/common';
- 
-console.log(name);
-console.log(`Привет Вебпак`);
