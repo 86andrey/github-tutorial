1107f3d8c45248762b9ce6d7b928ebbf8381c041
Andrey Ivanov <prioritet2503@gmail.com>
2022-07-17 22:23:18 +0300

    save

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-const startBtn = document.querySelector(".js-start");
-const stopBtn = document.querySelector(".js-stop");
-let timerId = null;
+// const startBtn = document.querySelector(".js-start");
+// const stopBtn = document.querySelector(".js-stop");
+// let timerId = null;
 
-startBtn.addEventListener("click", () => {
-  timerId = setInterval(() => {
-    console.log(`I love async JS!  ${Math.random()}`);
-  }, 1000);
+// startBtn.addEventListener("click", () => {
+//   timerId = setInterval(() => {
+//     console.log(`I love async JS!  ${Math.random()}`);
+//   }, 1000);
+// });
+
+// stopBtn.addEventListener("click", () => {
+//   clearInterval(timerId);
+//   console.log(`Interval with id ${timerId} has stopped!`);
+// });
+// const date = new Date();
+
+// console.log(date);
+// // "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
+
+// console.log(date.toString());
+// // "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
+
+const textInput = document.querySelector(".text-input");
+const output = document.querySelector(".output");
+let a = "";
+textInput.addEventListener("input", (event) => {
+  a = event.currentTarget.value;
+  // console.log(a);
+  output.textContent = event.currentTarget.value;
 });
+// console.log(a);
 
-stopBtn.addEventListener("click", () => {
-  clearInterval(timerId);
-  console.log(`Interval with id ${timerId} has stopped!`);
-});
-const date = new Date();
+console.log(a);
 
-console.log(date);
-// "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
-
-console.log(date.toString());
-// "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
+// console.log(a);
