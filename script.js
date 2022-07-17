80669fc2ce7a34a776d4588b2f6983c186b225b4
Andrey Ivanov <prioritet2503@gmail.com>
2022-07-17 22:47:15 +0300

    save

--- script.js
+++ script.js
@@ -20,16 +20,27 @@
 // console.log(date.toString());
 // // "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
 
-const textInput = document.querySelector(".text-input");
-const output = document.querySelector(".output");
-let a = "";
-textInput.addEventListener("input", (event) => {
-  a = event.currentTarget.value;
-  // console.log(a);
-  output.textContent = event.currentTarget.value;
-});
+// const textInput = document.querySelector(".text-input");
+// const output = document.querySelector(".output");
+// let a = "";
+// textInput.addEventListener("input", (event) => {
+//   a = event.currentTarget.value;
+//   // console.log(a);
+//   output.textContent = event.currentTarget.value;
+// });
 // console.log(a);
 
-console.log(a);
-
 // console.log(a);
+const a = (number) =>
+  Math.abs(number)
+    .toString()
+    .split("")
+    .reduce((sum, cur) => +sum + +cur, 0);
+console.log(a(25));
+
+const b = (num) =>
+  Math.abs(num)
+    .toString()
+    .split("")
+    .reduce((acc, num) => +acc + +num, 0);
+console.log(b(56));
